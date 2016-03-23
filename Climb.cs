3641ed5c925c6d9aa3bbb18be58d53c1be05a09b
ken-kentan <ken.kentan.k2@gmail.com>
2016-03-23 23:52:37 +0900

    add Climb.cs

---
+++ Climb.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Climb : MonoBehaviour
+{
+
+    public bool isCube;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (World.isPause) return;
+
+        if (isCube)
+        {
+            CubeManager.cubeBody.drag = 5;
+            CubeManager.ResetJump();
+        }
+        else
+        {
+            CubeManager.cubeBody.drag = 0.5f;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Cube") isCube = true;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Cube") isCube = false;
+    }
+}
